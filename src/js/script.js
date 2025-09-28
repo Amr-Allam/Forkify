f7dc7eb606dbeb286a3dff10d71a039758287e03
Amr Allam <115567652+Amr-Allam@users.noreply.github.com>
2025-09-28 04:37:21 +0300

    add search results

--- src/js/script.js
+++ src/js/script.js
@@ -14,24 +14,28 @@ const timeout = function (s) {
   });
 };
 
-const recipeContainer = document.querySelector(".recipe");
+const recipe = document.querySelector(".recipe");
 const searchBar = document.querySelector(".search-bar");
 const searchButton = document.querySelector(".search-button");
+const recipesContainer = document.querySelector(".recipes-container");
 
 const getRecipe = async function (e) {
   e.preventDefault();
-  const searchValue = searchBar.value;
+  const searchValue = searchBar.value.toLowerCase();
 
   try {
     const res = await fetch(
-      "https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886"
-      // `https://forkify-api.jonas.io/api/v2/recipes?search=${searchValue}`
+      // "https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886"
+      `https://forkify-api.jonas.io/api/v2/recipes?search=${searchValue}`
     );
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
     console.log(res, data);
+
+    const { recipes } = data.data;
+    renderRecipes(recipes.slice(10));
 
     // let { recipe } = data.data;
     // console.log(recipe);
@@ -52,3 +56,23 @@ const getRecipe = async function (e) {
 };
 
 searchButton.addEventListener("click", getRecipe);
+
+const renderRecipes = function (recipes) {
+  recipes.forEach((recipe) => {
+    const html = `
+            <li>
+              <a href="" class="result">
+                <img src="${recipe.image_url}" alt="recipe" class="result-icon" />
+                <div class="result-details">
+                  <h4>${recipe.title}</h4>
+                  <p>${recipe.publisher}</p>
+                </div>
+              </a>
+            </li>
+    `;
+    console.log(recipe.image_url);
+    recipesContainer.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+// parcel index.html --https false
