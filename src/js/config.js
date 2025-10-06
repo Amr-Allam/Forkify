348fa4a26244817b32677323f064e0edd9af3c5f
Amr Allam <115567652+Amr-Allam@users.noreply.github.com>
2025-10-06 04:01:50 +0300

    increase timeout

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.jonas.io/api/v2/recipes/";
-export const TIMEOUT_SEC = 20;
+export const TIMEOUT_SEC = 120;
 export const RES_PER_PAGE = 10;
 export const KEY = "edd02fee-860a-4f45-b287-43fbe51ff36e";
 export const MODAL_CLOSE_SEC = 2.5;
