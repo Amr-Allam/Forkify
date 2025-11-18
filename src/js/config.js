767e3d61908c96f5f7dce1b8847cac3186e77272
Amr Allam <115567652+Amr-Allam@users.noreply.github.com>
2025-11-18 02:35:36 +0200

    change api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.jonas.io/api/v2/recipes/";
 export const TIMEOUT_SEC = 120;
 export const RES_PER_PAGE = 10;
-export const KEY = "edd02fee-860a-4f45-b287-43fbe51ff36e";
+export const KEY = "cbfe4d79-c92d-4be3-aa11-12e9a43714be";
 export const MODAL_CLOSE_SEC = 2.5;
